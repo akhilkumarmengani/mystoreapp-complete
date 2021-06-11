d93da1611999cf40427c332aed94068af4da91dd
akhilkumarmengani <akhilkumarmengani@gmail.com>
2021-06-11 18:09:44 +0530

    deploy eb

--- mystore-api/bin/deploy.sh
+++ mystore-api/bin/deploy.sh
@@ -1,5 +1,7 @@
 echo "Hello!"
 
+aws configure --profile akhilkumarm
+
 eb setenv POSTGRES_HOST=mystore.cuszxooszhyd.us-east-1.rds.amazonaws.com POSTGRES_DB=postgres POSTGRES_USER=postgres POSTGRES_PASSWORD=akhil123 POSTGRES_DB_TEST=storefront_test NODE_ENV=dev PORT=5432 TOKEN_SECRET=I_AM_THE_LAST_JEDI SALT_ROUNDS=10 BCRYPT_PASSWORD=UNBREAKABLE
  TEST_TOKEN=eyJhbGciOiJIUzI1NiJ9.MQ.SMntCq5FXdlRgXKQZztNsTeecF7EdLXPeUpop53SUlY
 
